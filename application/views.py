d04eacd2d85300c555233bb1661322527bb36805
velllum <velllum@yandex.ru>
2021-11-18 16:38:26 +0300

    реализация jwt token

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from flask import (
     current_app as app,
     request,
     url_for,
-    views, make_response, jsonify,
+    views, make_response, jsonify, session,
 )
 from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token, JWTManager, get_jwt_identity
 
@@ -34,9 +34,9 @@ class Index(views.MethodView):
             'access_token': access_token,
         }
 
-        flask.session["Authorization"] = f"Bearer {access_token}"
+        session["Authorization"] = f"Bearer {access_token}"
 
-        print(flask.session.get("Authorization"))
+        print(session.get("Authorization"))
 
         # return make_response(jsonify(counter), 200)
         # return jsonify(access_token=counter)
@@ -53,18 +53,18 @@ class Index(views.MethodView):
 class User(views.MethodView):
     """- пользователь"""
 
-    # decorators = [jwt_required()]
+    decorators = [jwt_required()]
 
     def get(self):
-        # user = get_jwt_identity()
+        user = get_jwt_identity()
 
         counter = {
             "user": 'user',
-            'access_token': flask.session.get("Authorization"),
+            'access_token': session.get("Authorization"),
         }
 
         response = make_response(jsonify(counter))
-        response.headers["Authorization"] = flask.session.get("Authorization")
+        response.headers["Authorization"] = session.get("Authorization")
         return response
 
 
